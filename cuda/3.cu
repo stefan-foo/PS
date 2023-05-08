01aa5e003ec77e55dd889e25ac28ac0b8584a813
stefan-foo <stojadinovicstefan59@gmail.com>
2023-05-09 00:35:32 +0200

    3. zad

--- cuda/3.cu
+++ cuda/3.cu
@@ -1,15 +1,13 @@
 %%cu
 #include <vector>
 #include <iostream>
-#define N 125
-#define M 256
-#define BLOCK_DIM 256
+#define N 15
+#define M 300
+#define BLOCK_DIM 32
 #define REDUCE_SIZE (M + BLOCK_DIM - 1) / BLOCK_DIM
-#define MAX_GRID_SIZE 256
-#define SHMEM_SIZE 4 * BLOCK_DIM
 
 __global__ void reduceRowSum(int* v, int* v_r, int n, int m) {
-  __shared__ int partial_sum[BLOCK_DIM][SHMEM_SIZE];
+  __shared__ int partial_sum[BLOCK_DIM][BLOCK_DIM];
 
   int tx = blockIdx.x * blockDim.x * 2 + threadIdx.x;
   int ty = blockIdx.y * blockDim.y + threadIdx.y;
@@ -17,7 +15,7 @@ __global__ void reduceRowSum(int* v, int* v_r, int n, int m) {
   if (ty < n && tx < m) {
     partial_sum[threadIdx.y][threadIdx.x] = v[ty * m + tx];
     if (tx + blockDim.x < m) {
-        partial_sum[threadIdx.y][threadIdx.x] += v[ty * m * tx + blockDim.x];
+        partial_sum[threadIdx.y][threadIdx.x] += v[ty * m + tx + blockDim.x];
     }
   } else {
     partial_sum[threadIdx.y][threadIdx.x] = 0;
@@ -40,40 +38,32 @@ __global__ void reduceRowSum(int* v, int* v_r, int n, int m) {
 int main(void)
 {
   int matrix[N][M];
-  int btresult[N][(M + BLOCK_DIM - 1) / BLOCK_DIM];
+  int btresult[N][M];
   int bytes = N * M * sizeof(int);
   
   for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      matrix[i][j] = 1;
+    for (int j = 0; j < M; j++) {
+      matrix[i][j] = i % 2;
     }
   }
 
   int* d_matrix, *d_btresult;
   cudaMalloc((void**)&d_matrix, bytes);
-  cudaMalloc((void**)&d_btresult, N * REDUCE_SIZE);
+  cudaMalloc((void**)&d_btresult, bytes);
 
   cudaMemcpy(d_matrix, matrix, bytes, cudaMemcpyHostToDevice);
+  reduceRowSum<<<dim3((M + BLOCK_DIM * 2 - 1) / (BLOCK_DIM * 2), (N + BLOCK_DIM - 1) / BLOCK_DIM), 
+                  dim3(BLOCK_DIM, BLOCK_DIM)>>>(d_matrix, d_btresult, N, M); 
+  reduceRowSum<<<dim3(1, (N + BLOCK_DIM - 1) / BLOCK_DIM), dim3((M + BLOCK_DIM * 2 - 1) / (BLOCK_DIM * 2), BLOCK_DIM)>>>(d_btresult, d_btresult, N, M);
 
-  reduceRowSum<<<dim3((M + BLOCK_DIM - 1) / BLOCK_DIM / 2, (N + BLOCK_DIM - 1) / BLOCK_DIM), 
-                  dim3(BLOCK_DIM, BLOCK_DIM)>>>(d_matrix, d_btresult); 
-  reduceRowSum<<<dim3(1, (N + BLOCK_DIM - 1) / BLOCK_DIM), REDUCE_SIZE>>>(d_btresult, d_btresult);
+  cudaMemcpy(btresult, d_btresult, bytes, cudaMemcpyDeviceToHost);
 
+  int result[N];
   for (int i = 0; i < N; i++) {
-    std::cout << d_btresult[i * M] << std::endl;
+    result[i] = btresult[i][0];
+    std::cout << i << " " << btresult[i][0] << std::endl;
   }
 
-  int* d_v_r;
-  cudaMalloc((void**)&d_v_r, sizeof(int) * N);
-  
-  int GRID_DIM = (N + BLOCK_DIM * 2 - 1) / (BLOCK_DIM * 2);
-  reduceSum<<<GRID_DIM, BLOCK_DIM>>>(d_c_s, d_v_r, N);
-  reduceSum<<<1, GRID_DIM>>>(d_v_r, d_v_r, GRID_DIM);
-
-  int result;
-  cudaMemcpy(&result, d_v_r, sizeof(int), cudaMemcpyDeviceToHost);
-
-  std::cout << result << std::endl;
 
   // cudaFree(d_a);
   // cudaFree(d_b);
